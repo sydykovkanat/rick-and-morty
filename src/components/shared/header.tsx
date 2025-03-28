b5cc93cb36eeeb31e220c205d8201b20596676f5
sydykovkanat <sydykovkanat07@icloud.com>
2025-03-28 11:06:13 +0600

    add header description

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export function Header() {
 	return (
-		<header className={'flex h-[60px] justify-center'}>
-			<Link href={'/'} className={'flex items-center gap-2'}>
+		<header className={'flex flex-col mb-4 gap-y-1 justify-center'}>
+			<Link href={'/'} className={'flex items-center justify-center gap-2'}>
 				<Image
 					src={'/icon.svg'}
 					alt={'logo icon'}
@@ -12,11 +12,12 @@ export function Header() {
 					height={32}
 					priority
 				/>
-
-				<h1 className={'text-xl font-semibold flex items-center'}>
-					Rick And Morty
-				</h1>
+				<h1 className={'text-xl font-semibold'}>Rick And Morty</h1>
 			</Link>
+
+			<p className={'text-sm text-muted-foreground text-center'}>
+				Find your favorite characters Rick and Morty
+			</p>
 		</header>
 	);
 }
