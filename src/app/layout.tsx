1a606c5df02ac02d0435c368ccfe25b55ef0bb07
sydykovkanat <sydykovkanat07@icloud.com>
2025-03-31 11:13:48 +0600

    setup metadatas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,29 @@ export const metadata: Metadata = {
 	},
 	description: SITE_DESCRIPTION,
 	openGraph: {
-		images: '/aidaika.jpg',
-		title: SITE_NAME,
+		title: 'Rick & Morty: Кто Ты Сегодня?',
 		description:
-			'Айдусечка - милая и умная девочка, с которой очень интересно общаться!',
+			'Исследуйте вселенную Рика и Морти! Найдите персонажей по статусу и гендеру.',
+		url: 'https://your-domain.com',
+		siteName: 'Rick & Morty Finder',
+		images: [
+			{
+				url: 'https://cdn4.cdn-telegram.org/file/ClADK7J7RYR68ogPHICXppHv25mYKbfdUlddV7iJpReSAWNdLuIdR0fSzFhVjwl2Nzi_3jRn_KMqBV4k6Np4xgwD6i7Covz9TVuChJms1A3GoknabEECCgx8VfGW1mg6cR05WSNOMe3M03otwoGr2ir0NmiEnOuhtHaZ6_WUCsSV1sw8V36WkaU_8tKWGQXH6MSzzm3-YZ5NwEsyV2CmElSzH9WkYk5FrVmb7J1J0DFE3b_wl-U1yJdWT59OQrtBQfn1ic0TAgcly3A9mrdJGa4VUAzrvQVP5Hv_RrODwybW7veiGi91g3zVBYK1DX1RtEOatOskfZv0iNxDeHQ2lw.jpg',
+				width: 630,
+				height: 630,
+				alt: 'Rick and Morty characters',
+			},
+		],
+		type: 'website',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Rick & Morty: Кто Ты Сегодня?',
+		description:
+			'Исследуйте вселенную Рика и Морти! Найдите персонажей по статусу и гендеру.',
+		images: [
+			'https://cdn4.cdn-telegram.org/file/ClADK7J7RYR68ogPHICXppHv25mYKbfdUlddV7iJpReSAWNdLuIdR0fSzFhVjwl2Nzi_3jRn_KMqBV4k6Np4xgwD6i7Covz9TVuChJms1A3GoknabEECCgx8VfGW1mg6cR05WSNOMe3M03otwoGr2ir0NmiEnOuhtHaZ6_WUCsSV1sw8V36WkaU_8tKWGQXH6MSzzm3-YZ5NwEsyV2CmElSzH9WkYk5FrVmb7J1J0DFE3b_wl-U1yJdWT59OQrtBQfn1ic0TAgcly3A9mrdJGa4VUAzrvQVP5Hv_RrODwybW7veiGi91g3zVBYK1DX1RtEOatOskfZv0iNxDeHQ2lw.jpg',
+		],
 	},
 };
 
